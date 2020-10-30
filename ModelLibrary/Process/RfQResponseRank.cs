52bb7a9c6bb3fca8645818907c6f01bbf57b261b
Bharat Singla <bharat.singla@viennasolutions.com>
2020-10-30 10:38:01 +0530

    set last amount to zero in RFQ Rank

--- ModelLibrary/Process/RfQResponseRank.cs
+++ ModelLibrary/Process/RfQResponseRank.cs
@@ -187,7 +187,7 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
                                 // JID_1064  only Submitted responce will set rank
                                 if(lastAmt.Value.Equals(Env.ZERO))
                                 {
-                                    lastRank = rank;
+                                    lastAmt = qty.GetNetAmt();
                                 }
                                 else if (lastAmt.Value.CompareTo(netAmt.Value) != 0)
                                 {
